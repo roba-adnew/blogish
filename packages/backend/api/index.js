3290d9ac998ee2c0345a545b20e43339c8a3c6cd
roba <10408936+roba-adnew@users.noreply.github.com>
2024-07-23 00:42:00 -0400

    Implemented simple router for testing

--- packages/backend/api/index.js
+++ packages/backend/api/index.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
 const express = require('express')
+const authRouter = require('../src/routes/auth')
 
 const app = express()
 app.use(express.json())
 
-app.use('/', (req, res) => res.send('this is up at least') )
+app.use('/user', authRouter)
+app.use('/', (req, res) => res.send('this is up at least'))
+
 app.listen(4000, () => console.log('running on 4k'))
 
 module.exports = app;
