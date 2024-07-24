07aadbbb1846471e7e8a31ac251185075cb79c42
roba <10408936+roba-adnew@users.noreply.github.com>
2024-07-23 23:21:16 -0400

    updating the alias and build entry point for shared dir

--- packages/shared/vite.config.js
+++ packages/shared/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'Components/'),
+      entry: path.resolve(__dirname, 'src/'),
       name: 'SharedComponents',
       fileName: (format) => `shared-components.${format}.js`
     },
