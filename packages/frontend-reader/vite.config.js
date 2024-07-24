f47cccbe10f990a372d7c7b7b46e3540ebc4d8d5
roba <10408936+roba-adnew@users.noreply.github.com>
2024-07-24 14:31:28 -0400

    Adding rollup options to explicitly frontend reader

--- packages/frontend-reader/vite.config.js
+++ packages/frontend-reader/vite.config.js
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'shared': '../shared/Components'
+  build: {
+    rollupOptions: {
+      external: ['date-fns'],
+      output: {
+        globals: {
+          dateFns: 'date-fns'
+        }
+      }
     }
   },
   server: {
