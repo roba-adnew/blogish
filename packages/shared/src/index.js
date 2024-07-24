fe344003b89adb958eb5ae2255da2b8cbd813e04
roba <10408936+roba-adnew@users.noreply.github.com>
2024-07-23 23:42:48 -0400

    updating entry point for shared components in vite.config

---
+++ packages/shared/src/index.js
@@ -0,0 +1,5 @@
+export { default as Login } from './Components/Login'
+export { default as NavBar } from './Components/NavBar'
+export { default as SignUp } from './Components/SignUp'
+export { signUp, login, deleteRefreshToken, fetchWithToken }  from './utils/authApi'
+
--- packages/shared/vite.config.js
+++ packages/shared/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/'),
+      entry: path.resolve(__dirname, 'src/index.js'),
       name: 'SharedComponents',
       fileName: (format) => `shared-components.${format}.js`
     },
