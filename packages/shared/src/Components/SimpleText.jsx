d3aa7471aa1bf56e5a31922658b6bbbc1ed0178b
roba <10408936+roba-adnew@users.noreply.github.com>
2024-07-25 13:33:50 -0400

    fixing shared declarations

---
+++ packages/shared/src/Components/SimpleText.jsx
@@ -0,0 +1,10 @@
+function SimpleText({ title, body }) {
+    return (
+        <>
+            <h1>{title}</h1>
+            <p>{body}</p>
+        </>
+    )
+}
+
+export default SimpleText;
--- packages/shared/vite.config.js
+++ packages/shared/vite.config.js
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     'shared': '../shared/Components'
-  //   }
-  // },
-  server: {
-    port: 5000,
+  build: {
+    rollupOptions: {
+      external: ['date-fns'],
+      output: {
+        globals: {
+          dateFns: 'date-fns'
+        }
+      }
+    }
   }
 })
